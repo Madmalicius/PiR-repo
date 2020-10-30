e36d3407f1fba6c025698bd8f5d56735c51ac6a6
Milan <mhtechdev@gmail.com>
2020-10-30 17:04:22 +0000

    Removing build files, should not be committed

--- catkin_ws/build/hole_detector/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikkel/PiR-repo/catkin_ws/src/hole_detector
-# Build directory: /home/mikkel/PiR-repo/catkin_ws/build/hole_detector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
