486743b23e7495be4d5f2ae6abb3c0682ff994b5
Milan <mhtechdev@gmail.com>
2020-11-12 12:13:39 +0000

    Camera adjustment

--- catkin_ws/src/hole_detector/src/camera_controller.py
+++ catkin_ws/src/hole_detector/src/camera_controller.py
@@ -8,6 +8,7 @@ import os
 import time
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import os.path
 
 class CameraController():
 
@@ -18,18 +19,18 @@ class CameraController():
         self.cam = PiCamera()
         self.cam.resolution = (4056, 3040)
         self.cam.framerate = 5
-        self.cam.sharpness = 80
-        self.cam.iso = 0 #800
-        self.cam.shutter_speed = 0      # shutter, or 0 for auto
-        self.cam.image_effect = 'none'  # sketch, denoise, cartoon
-        self.cam.exposure_mode = 'auto'  # auto, night, spotlight, sports, snow, beach, antishake, fireworks
-        self.cam.exposure_compensation = 0  #-25 - 25
-        self.cam.contrast = 0           # -100 - 100
-        self.cam.awb_mode = 'auto'      # off, auto, sunlight, cloudy, shade, tungsten, flourescent, incandescent, flash, horizon
-        self.cam.image_denoise = True
-        self.cam.meter_mode = 'backlit'
-        self.cam.saturation = 0         # -100 - 100
-        time.sleep(2)
+        self.cam.sharpness = 50
+        #self.cam.iso = 100
+        #self.cam.shutter_speed = 0#200      # shutter, or 0 for auto
+        #self.cam.image_effect = 'none'  # sketch, denoise, cartoon
+        #self.cam.exposure_mode = 'auto'  # auto, night, spotlight, sports, snow, beach, antishake, fireworks
+        #self.cam.exposure_compensation = 0  #-25 - 25
+        #self.cam.contrast = 0           # -100 - 100
+        #self.cam.awb_mode = 'auto'      # off, auto, sunlight, cloudy, shade, tungsten, flourescent, incandescent, flash, horizon
+        #self.cam.image_denoise = True
+        #self.cam.meter_mode = 'backlit'
+        #self.cam.saturation = 0         # -100 - 100
+        time.sleep(6.0)
 
     def capture(self):
         img = np.empty((4064 * 3040 * 3), dtype=np.uint8)
@@ -45,15 +46,28 @@ class CameraController():
         return img
 
 if __name__ == '__main__':
+    path = "/home/ubuntu/fence_imgs/"
     cc = CameraController()
     #rospy.init_node("camera_controller")
 
-    t = time.time()
-    img = cc.capture()
-    print("Get img took ", time.time() - t, " seconds")
-    t = time.time()
-    cv2.imwrite("/home/ubuntu/fence_imgs/img_1.jpg", img)
-    print("Writing img took ", time.time() - t, " seconds")
+    for i in range(0, 1):
+
+        t = time.time()
+        img = cc.capture()
+        print("Get img took ", time.time() - t, " seconds")
+
+        cnt = 0
+
+        while True:
+            fpath = path + "img_" + str(cnt) + ".jpg"
+            if os.path.isfile(fpath):
+                cnt += 1
+            else:
+                break
+
+        t = time.time()
+        cv2.imwrite(path + "img_" + str(cnt) + ".jpg", img)
+        print("Writing img took ", time.time() - t, " seconds")
 
     # Loop
     #while(not rospy.is_shutdown()):
