1b91df06ee8c88461023e84e644f6a6ac9feb1f2
Mikkel Olesen <rasmus@hvilshoej.dk>
2020-11-12 03:34:30 -0800

    Add setup & startup scripts

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+sudo apt install ros-melodic-mavros ros-melodic-mavros-extras -y
+
+pushd ./catkin_ws
+    catkin build
+    chmod +x ./src/hole_detector/src/offb_control.py
+popd
+
+pwd
+
+pushd ~
+    curl https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh | sudo bash install_geographiclib_datasets.sh 
+popd
+
+if [ -d ~/eit_ws/src ]; then
+    echo "eit_ws already exists"
+else
+    source /opt/ros/melodic/setup.bash
+    mkdir -p ~/eit_ws/src
+    pwd
+    pushd ~/eit_ws/src/
+        catkin_init_workspace
+    popd
+    pushd ~/eit_ws/
+        catkin build
+        sudo apt-get install unzip
+    popd
+    pushd ~/eit_ws/src
+        wget https://nextcloud.sdu.dk/index.php/s/oycQ3pgQPXASHEY/download
+        unzip download
+        rm download
+    popd
+fi
+
+cp -rf ./sdu_drone_mono_cam.sdf ~/eit_ws/src/eit_playground/models/sdu_drone_mono_cam
+
+if [ -f ~/eit_ws/src/eit_playground/worlds/hca_airport_fence_complex.world ]; then
+    echo "World file already exists"
+else
+    pushd ~/eit_ws/src/eit_playground/worlds/
+        wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1b8MNieB_JLFnwehBVa12ll2kzuR6cK6_' -O hca_airport_fence_complex.world
+    popd
+fi
+
+bash start.sh
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /home/$USER/Firmware/Tools/setup_gazebo.bash /home/$USER/Firmware /home/$USER/Firmware/build/px4_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/Firmware
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/Firmware/Tools/sitl_gazebo
+
+gnome-terminal --window -- bash -c "roslaunch mavros px4.launch fcu_url:='udp://:14540@127.0.0.1:14557'"
+
+gnome-terminal --window -- bash -c "source ~/eit_ws/devel/setup.bash && source ~/eit_ws/src/eit_playground/setup_gazebo.bash && cd ~/eit_ws/ && (catkin build || catkin_make) ; roslaunch eit_playground posix.launch vehicle:=sdu_drone_mono_cam env:=hca_airport_fence_complex"
