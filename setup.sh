6faf112f97e45a1d0a0072fd46cb3008c9755633
Rasmus Henriksen <rasmus@hvilshoej.dk>
2020-11-19 14:30:47 +0100

    echo user

--- setup.sh
+++ setup.sh
@@ -86,5 +86,6 @@ fi
 ln -s /home/$USER/eit_ws/src/eit_playground/init.d-posix/* /home/$USER/Firmware/ROMFS/px4fmu_common/init.d-posix/
 ln -s /home/$USER/eit_ws/src/eit_playground/mixers/* /home/$USER/Firmware/ROMFS/px4fmu_common/mixers/
 
+echo $USER
 
 bash start.sh
